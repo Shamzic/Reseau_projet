954a5cd3dc53a7dfb43fcda1862e6e6d2f9f991a
shamzic <simon.hamery2@etu.unistra.fr>
2016-11-19 17:19:08 +0100

    exo1 chunk de fin ok

--- exo1.c
+++ exo1.c
@@ -1,7 +1,7 @@
 /*  
 -----------------------------
 Test du fichier : 
- >gcc exo1.c sha256.c -o main
+ >gcc exo1.c sha256.c -o main -Wall -Werror -Wextra
  >./main toto
 -----------------------------
 */
@@ -63,57 +63,25 @@ int main (int argc, char * argv[])
   //buffer = (char*)malloc(BUFFER_SIZE);
   int i=1;
   
-  while((taille = read(file,buffer,BUFFER_SIZE)) > 0) // parcourt le fichier 10 octets par 10 pour l'instant
+  while((taille = read(file,buffer,BUFFER_SIZE)) > 0) 
+  // parcourt le fichier 10 octets par 10 pour l'instant
   {
       //suppr_retour_chariot(buffer);
-      printf("taille %d ",taille);
+     // printf("taille %d ",taille);
       buffer[taille]='\0';
-      printf("chunk %d : %s\n",i,buffer);       
+      //printf("chaine %d : %s\n",i,buffer);       
       sha256_init(&ctx);
-      sha256_update(&ctx,buffer,taille);
+      if (taille == BUFFER_SIZE)
+        sha256_update(&ctx,buffer,taille);
+      else if (taille < BUFFER_SIZE)
+        sha256_update(&ctx,buffer,taille-1);
       sha256_final(&ctx,hash);
+      printf("chunk %d : ",i);
       print_hash(hash);
       i++;     
   } 
   //free(buffer); 
   if(close(file) == -1)
     msg_error("close");
-    
-//---------------------------------------------------------
-// version avec fonction bibliothèques fgets fopen fclose
-// --------------------------------------------------------
-   /*
-  unsigned char hash2[32];
-  SHA256_CTX ctx2;
-    
-    
-  FILE* fichier = NULL;
-  fichier = fopen(argv[1], "r+");
-  char buff[BUFFER_SIZE];
-  if (fichier != NULL)
-  {// On peut lire et écrire dans le fichier
-  int j=1;
-    while(fgets(buff,BUFFER_SIZE+1,fichier)!=NULL)
-    {
-      //if(buff[strlen(buff)]='\n')
-      //{  
-        printf("caractètre de fin de chaine de buff :%c.\n",buff[strlen(buff)]);
-        //buff[strlen(buff)]='\0';
-      //}
-      sha256_init(&ctx2);
-      sha256_update(&ctx2,buff,BUFFER_SIZE);
-      sha256_final(&ctx2,hash2);
-      printf("chunk %d : %s.\n",j,buff); 
-      print_hash(hash2);
-      j++;
-    }
-  
-    fclose(fichier);
-  }
-  else
-  {
-// On affiche un message d'erreur si on veut
-    printf("Impossible d'ouvrir le fichier test.txt");
-  }*/
   return 0;
 }
